9c81a7cc6ddeeecec96b28db0c0ae82f7c1659af
wemrekurt <emre.kurt@bil.omu.edu.tr>
2017-10-05 15:01:44 +0300

    Category modeli oluşturuldu

---
+++ db/migrate/20171001220313_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :title
+      t.text :description
+      t.string :keywords
+      t.string :sef_link
+
+      t.timestamps
+    end
+  end
+end
