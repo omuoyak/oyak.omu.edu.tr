317d72247bc49aa37babe6d4af101dff607ff081
wemrekurt <emre.kurt@bil.omu.edu.tr>
2017-10-05 15:04:01 +0300

    Blog, yazı ve admin için route ayarlaması

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'yazi/:sef', to: 'home#post', as: 'show_post'
+  get 'blog', to: 'home#blog', as: 'show_blog'
+
+  scope :admin, module: :admin do
+    resources :posts
+  end
+
+
+
 end
