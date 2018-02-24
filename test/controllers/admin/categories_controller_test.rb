a0f791dc6ec66c2696a21a0be505501fa55f2766
ileri <celil.ileri@bil.omu.edu.tr>
2018-02-25 00:42:53 +0300

    oluşan test dosyalarını ekle

---
+++ test/controllers/admin/categories_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get admin_categories_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get admin_categories_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get admin_categories_edit_url
+    assert_response :success
+  end
+
+end
