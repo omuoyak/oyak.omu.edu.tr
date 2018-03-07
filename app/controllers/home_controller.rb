97eece1cab1a5b420ca448eb2d37fd92b6e1137c
ileri <celil.ileri@bil.omu.edu.tr>
2018-03-07 12:13:06 +0300

    anasayfaya önerilen makaleleri ekle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @latest_posts = latest_posts
     @latest_activities = latest_activities
+    @latest_suggestions = latest_suggestions
   end
 
   def docs
@@ -11,6 +12,11 @@ class HomeController < ApplicationController
   end
 
   private
+
+
+  def latest_suggestions
+    Rails.cache.fetch('latest_suggestions'){ Suggestion.last(5).reverse }
+  end
 
   def latest_posts
     Rails.cache.fetch('latest_posts'){ Post.where(approved: true).last(5).reverse }
