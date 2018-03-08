303fbe46977743b854c09bce5b92fe44324a3020
Emre KURT <emre.kurt@bil.omu.edu.tr>
2018-03-08 13:48:47 +0300

    önerilen makale CUD durumlarına anasayfada listelenenler cache temizle

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -3,8 +3,25 @@ class Suggestion < ApplicationRecord
 
   validates :title, presence: true
 
+  after_commit on: [:create] do
+    clear_caches
+  end
 
+  after_commit on: [:update] do
+    clear_caches
+  end
+
+  after_commit on: [:destroy] do
+    clear_caches
+  end
+
+  
   def has_link
     link && link.size > 0
+  end
+
+  private
+  def clear_caches
+    Rails.cache.delete('latest_suggestions')
   end
 end
